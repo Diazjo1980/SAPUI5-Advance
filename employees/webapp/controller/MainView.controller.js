dfd56308e87f72cb80bbe4c1d5bd387ee3682094
Diazjo1980 <diazmjorgea@gmail.com>
2021-11-24 01:10:13 +0000

    First commit to branch 04

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,29 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.employees.controller.MainView", {
-			onInit: function () {
+        var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
 
-			}
-		});
-	});
+        Main.prototype.onValidate = function() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+
+            } else {
+               // inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);                
+            }; 
+        }
+
+        return Main;
+    });
