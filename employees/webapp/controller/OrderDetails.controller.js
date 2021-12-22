4988be3a6401b6baeb6c3264b7f8f5c3bf9d0d05
Jorge Alberto Diaz Montilla <diazmjorgea@gmail.com>
2021-12-22 23:25:32 +0000

    Laboratorio 29 Factory

--- employees/webapp/controller/OrderDetails.controller.js
+++ employees/webapp/controller/OrderDetails.controller.js
@@ -33,6 +33,39 @@ sap.ui.define([
             signature.clear();
         };
 
+        function factoryOrderDetails (listId, oContext) {
+
+            let contextObject = oContext.getObject();
+            oContext.Currency = "EUR";
+            let unitsInStock = oContext.getModel().getProperty("/Products(" + contextObject.ProductID + ")/UnitsInStock");
+
+            if (contextObject.Quantity <= unitsInStock) {
+
+                let objectListItem = new sap.m.ObjectListItem({
+                    title: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})",
+                    number: "{ parts: [{path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type: 'sap.ui.model.type.Currency', formatOptions: {showMeasure: false}}",
+                    numberUnit: "{odataNorthwind>Currency}"
+                }); 
+                
+                return objectListItem;
+
+            } else {
+
+                let customListItem = new sap.m.CustomListItem({
+                    content : [
+                        new sap.m.Bar({
+                            contentLeft: new sap.m.Label({ text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})"}),
+                            contentMiddle: new sap.m.ObjectStatus({text: "{i18n>availableStatus} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state: 'Error'}),
+                            contentRight: new sap.m.Label({ text: "{ parts: [{path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type: 'sap.ui.model.type.Currency'}" }) 
+                        })
+                    ]
+                });
+
+                return customListItem;
+            }
+
+        };
+
         let OrderDetails = Controller.extend("logaligroup.employees.controller.OrderDetails", {
 
 
@@ -41,6 +74,7 @@ sap.ui.define([
             OrderDetails.prototype.onInit = onInit;
             OrderDetails.prototype.onBack = onBack;
             OrderDetails.prototype.onClearSignature = onClearSignature;
+            OrderDetails.prototype.factoryOrderDetails = factoryOrderDetails;
 
             return OrderDetails;
 });
